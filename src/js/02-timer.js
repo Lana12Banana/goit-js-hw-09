9d26f6e05a16a510520749958f7db350c342f5c0
Lana12Banana <114477280+Lana12Banana@users.noreply.github.com>
2023-02-23 10:29:13 +0200

    Uptade 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,75 +1,109 @@
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
-import "flatpickr/dist/flatpickr.min.css";
 
-const buttonTimerStart = document.querySelector("[data-start]")
-const inputTimer = document.getElementById("datetime-picker")
-const days = document.querySelector("[data-days]")
-const hours = document.querySelector("[data-hours]")
-const minutes = document.querySelector("[data-minutes]")
-const seconds = document.querySelector("[data-seconds]")
-buttonTimerStart.setAttribute("disabled", true);
-let chosenDate = null
+
+const refs = {
+  timerInput: document.querySelector('#datetime-picker'),
+  startButton: document.querySelector('button[data-start]'),
+  spanDays: document.querySelector('span[data-days]'),
+  spanHours: document.querySelector('span[data-hours]'),
+  spanMinutes: document.querySelector('span[data-minutes]'),
+  spanSeconds: document.querySelector('span[data-seconds]'),
+};
+
+refs.startButton.setAttribute('disabled', 'disabled');
+refs.startButton.addEventListener('click', onStartButtonClick);
+
+const dateNow = Date.now();
+
+let startTime = null;
 
 const options = {
-	enableTime: true,
-	time_24hr: true,
-	defaultDate: new Date(),
-	minuteIncrement: 1,
-	onClose(selectedDates) {
-		if (selectedDates[0].getTime() <= Date.now()) {
-			Notiflix.Notify.warning("Please choose a date in the future")
-			return
-		}
-		buttonTimerStart.removeAttribute("disabled", true);
-		buttonTimerStart.addEventListener("click", timerStart)
-		chosenDate = selectedDates[0];
-	},
-}
+  // enableTime
+  enableTime: true,
 
-function timerStart() {
-	buttonTimerStart.setAttribute("disabled", true);
-	inputTimer.setAttribute("disabled", true);
-	const interval = setInterval(() => {
-		const subtraction = chosenDate.getTime() - Date.now();
-		const timeConverter = convertMs(subtraction)
+  // Displays time picker in 24 hour mode without AM/PM selection when enabled.
+  time_24hr: true,
 
-		if (subtraction <= 0) {
-			clearInterval(interval)
-			return
-		}
+  // Sets the initial selected date(s)
+  defaultDate: new Date(),
 
-		days.textContent = timeConverter.days;
-		hours.textContent = timeConverter.hours;
-		minutes.textContent = timeConverter.minutes;
-		seconds.textContent = timeConverter.seconds;		
-	}, 1000)
-}
+  // Adjusts the step for the minute input (incl. scrolling)
+  minuteIncrement: 1,
 
-function addLeadingZero(value) {
-	return String(value).padStart(2, '0')
+  // Function to trigger on every time the calendar is closed.
+  onClose(selectedDates) {
+    const currentTime = Date.now();
+    let deltaTime = selectedDates[0] - currentTime;
+
+    if (selectedDates[0] < dateNow) {
+      // alert('Please choose a date in the future');
+      Notiflix.Notify.init({position: 'center-top'});
+      Notiflix.Notify.failure('Please choose a date in the future');
+
+      return;
+    }
+
+    refs.startButton.removeAttribute('disabled', 'disabled');
+
+    startTime = selectedDates[0];
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+
+    refs.spanDays.textContent = pad(Math.abs(days));
+    refs.spanHours.textContent = pad(Math.abs(hours));
+    refs.spanMinutes.textContent = pad(Math.abs(minutes));
+    refs.spanSeconds.textContent = pad(Math.abs(seconds));
+  },
+};
+
+const fp = flatpickr(refs.timerInput, options);
+
+function onStartButtonClick() {
+  refs.startButton.setAttribute('disabled', 'disabled');
+
+  const timerID = setInterval(() => {
+    const currentTime = Date.now();
+
+    let deltaTime = startTime - currentTime;
+
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+
+    refs.spanDays.textContent = pad(Math.abs(days));
+    refs.spanHours.textContent = pad(Math.abs(hours));
+    refs.spanMinutes.textContent = pad(Math.abs(minutes));
+    refs.spanSeconds.textContent = pad(Math.abs(seconds));
+
+    if (deltaTime < 0) {
+      clearInterval(timerID);
+      refs.spanDays.textContent = '00';
+      refs.spanHours.textContent = '00';
+      refs.spanMinutes.textContent = '00';
+      refs.spanSeconds.textContent = '00';
+    }
+  }, 1000);
 }
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
-    const second = 1000;
-	const minute = second * 60;
-	const hour = minute * 60;
-	const day = hour * 24;
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
   // Remaining days
-    const days = addLeadingZero(Math.floor(ms / day));
+  const days = Math.floor(ms / day);
   // Remaining hours
-    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const hours = Math.floor((ms % day) / hour);
   // Remaining minutes
-    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const minutes = Math.floor(((ms % day) % hour) / minute);
   // Remaining seconds
-    const seconds = addLeadingZero(
-    Math.floor((((ms % day) % hour) % minute) / second)
-    );
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-    return { days, hours, minutes, seconds };
+  return { days, hours, minutes, seconds };
 }
 
-flatpickr(inputTimer, options);
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
 
